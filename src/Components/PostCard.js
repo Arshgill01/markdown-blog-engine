bcae4817be1a1a3d70e0f4058e408f9bfbbe25fc
Arshdeep singh <arshgill6120@gmail.com>
2025-11-01 11:43:40 +0530

    Added a button to open the blogpost

--- src/Components/PostCard.js
+++ src/Components/PostCard.js
@@ -1,9 +1,12 @@
 import styles from "./PostCard.module.css"
 function PostCard(props){
-  const {title, date} = props;
+  const {title, date, slug} = props;
   return(
     <div className={styles.PostCard}>
       <h2 className={styles.PostTitle}>{title}</h2>
+      <button onClick={()=>{
+
+      }}>Open</button>
       <date className={styles.PostDate}>{date}</date>
     </div>
   );
